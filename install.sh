db6658932ec6aa3d99f85558ec6f9217bc93e001
hakansabol <hakansabol@gmail.com>
2026-02-06 13:46:58 -0800

    fixed install script

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 mkdir ../addons 2> /dev/null
 
-if [ ! -d "$../addons/richtext2" ]; then
+if [ ! -d "../addons/richtext2" ]; then
 	unzip -q GodotRichTextLabel2.zip
 	mv addons/richtext2 ../addons/
 	rm addons/ -r
+fi
+
+if [ ! -d "../sprites" ]; then
+	mkdir ../sprites
+fi
+if [ ! -d "../scenes" ]; then
+	mkdir ../scenes
+fi
+if [ ! -d "../sounds" ]; then
+	mkdir ../sounds
+fi
+if [ ! -d "../scripts" ]; then
+	mkdir ../scripts
 fi
