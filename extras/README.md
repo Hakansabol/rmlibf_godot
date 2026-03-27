84bed594375d5ca5d93f1b3053ab9417324f72d0
hakansabol <hakansabol@gmail.com>
2026-03-26 19:30:46 -0700

    Template files
    added a template game_manager that is auto installed because making one manually each project is annoying

---
+++ extras/README.md
@@ -0,0 +1,3 @@
+# Extras
+Some files here are copied by the initialization script, such as `game_manager`, which becomes `game_manager.gd`.
+Other files may be added in the future.
--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# lazy check if this is the right directory, this does have the caveat of working in subdirectories but like don't do that :\
+if [ ! $(pwd | grep rmlibf) ]; then
+	echo -e "\e[31merror: \e[0mplease run from the rmlibf_godot directory."
+	exit 1
+fi
+
+# create the destination folder for unzip.
 mkdir ../addons 2> /dev/null
 
 if [ ! -d "../addons/richtext2" ]; then
@@ -6,6 +15,7 @@ if [ ! -d "../addons/richtext2" ]; then
 	rm addons/ -r
 fi
 
+# these folders are created conditionally rather than supressing the error stream, I don't remember why.
 if [ ! -d "../sprites" ]; then
 	mkdir ../sprites
 fi
@@ -18,3 +28,7 @@ fi
 if [ ! -d "../scripts" ]; then
 	mkdir ../scripts
 fi
+
+# Spawn other files.
+# This is a template, meaning it is meant to be edited for this individual project, unlike most other scripts, which are meant to be extended and left unchanged.
+cp ./extras/game_manager ../scripts/game_manager.gd --no-clobber
