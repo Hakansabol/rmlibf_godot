3daf3e48ad24c5ba40e6164f7907383e294b2bc7
Hakan Sabol <hakansabol@gmail.com>
2026-08-25 19:52:47 -0700

    no more symlinks

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ if [ ! $(pwd | grep rmlibf) ]; then
 	exit 1
 fi
 
-# create the destination folder for unzip.
-mkdir ../addons 2> /dev/null
+mkdir ../scripts ../sprites ../sounds ../scenes ../addons 
 
 if [ ! -d "../addons/richtext2" ]; then
 	unzip -q GodotRichTextLabel2.zip
@@ -16,11 +15,10 @@ if [ ! -d "../addons/richtext2" ]; then
 fi
 
 # link all the folders into the root dir
-mkdir ../scripts ../sprites ../sounds ../scenes
-ln -sdrT scripts ../scripts/rml
-ln -sdrT sprites ../sprites/rml
-ln -sdrT shaders ../sprites/shaders
-ln -sdrT sounds ../sounds/rml
-ln -sdrT prefabs ../scenes/rml
+# ln -sdrT scripts ../scripts/rml
+# ln -sdrT sprites ../sprites/rml
+# ln -sdrT shaders ../sprites/shaders
+# ln -sdrT sounds ../sounds/rml
+# ln -sdrT prefabs ../scenes/rml
 
 echo -e "\e[32mreminder: \e[0mRegister scripts/game_manager.gd as an autoload under the name GameManager (likely default) to fix errors!"
